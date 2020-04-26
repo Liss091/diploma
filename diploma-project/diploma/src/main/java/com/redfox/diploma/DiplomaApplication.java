b6a5aba25e78e39d4b85d8f5d44bef186c271eea
DianaLysenko <redfox.diana@gmail.com>
2020-04-26 17:38:23 +0300

    jsf configuration

--- diploma-project/diploma/src/main/java/com/redfox/diploma/DiplomaApplication.java
+++ diploma-project/diploma/src/main/java/com/redfox/diploma/DiplomaApplication.java
@@ -2,12 +2,23 @@ package com.redfox.diploma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+
 
 @SpringBootApplication
 public class DiplomaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiplomaApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		return new ServletRegistrationBean(servlet, "*.jsf");
 	}
 
 }
